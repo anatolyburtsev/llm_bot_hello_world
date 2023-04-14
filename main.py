a20ec5e6f9d8db041b003f12c1e0691049cbbd75
Anatolii Burtsev <aburtsev@amazon.com>
2023-04-13 22:33:01 -0700

    add logging env vars

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 
 st.write("# Hello world!")
 st.write(f"env var: {os.environ.get('var')}")
+st.write(f"st secret var: {st.secrets['var']}")
 
 st.text_input("Input you text", key="first_text")
 
