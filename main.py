268924af288e3f28e0d6f8f44c1dd5b96070d095
Anatolii Burtsev <aburtsev@amazon.com>
2023-04-13 22:28:42 -0700

    super simple web ui with streamlit

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import os
+
+st.write("# Hello world!")
+st.write(f"env var: {os.environ.get('var')}")
+
+st.text_input("Input you text", key="first_text")
+
+st.write(st.session_state.first_text)
